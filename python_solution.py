3f2f08ad6b368b9a22e824e0a833ee3ebe993520
Shirzart Enwer <shirzartenwer@gmail.com>
2022-07-31 16:54:35 +0200

    feat: python script and linux command to process kindle file

---
+++ python_solution.py
@@ -0,0 +1,67 @@
+'''To import my kindle highlight to Readwise'''
+import enum
+import re 
+
+
+
+def get_kindle_highlights(dict_list, input_file_path, output_file_path):
+    with open(input_file_path) as infile, open(output_file_path, 'w') as outfile:
+        last_line_was_seperator = True
+        last_line_was_blank = False
+        last_line_was_info = False
+        for line in infile:
+            if last_line_was_seperator:
+                out_put_header["Title"].append(line)
+                last_line_was_seperator = False
+                continue
+            elif not last_line_was_blank and line.startswith("-"):
+                location = re.search('- (.*)|', line)
+                time = re.search('|(.*)', line)
+                out_put_header["Location"].append(location)
+                out_put_header["Date"].append(time)
+                last_line_was_blank = False
+                last_line_was_seperator = False
+                last_line_was_info = True
+                continue
+            elif line.strip() =="" and last_line_was_info:
+                last_line_was_blank = True
+                last_line_was_seperator = False
+                last_line_was_info = False
+                continue
+            elif last_line_was_blank:
+                out_put_header["Highlights"].append(line)
+                last_line_was_blank = False
+                continue     
+            elif line.strip() == "==========":
+                last_line_was_seperator = True
+                continue
+    return dict_list
+                
+                
+
+if __name__ == "__main__":
+    out_put_header = {"Highlights": [],
+                  "Title": [],
+                  "Author": [],
+                  "URL": [],
+                  "Note": [],
+                  "Location": [],
+                  "Date": []}
+    input_file_path = "../../MyClippings.txt"
+    output_intermediate = "../../intermidiate_kindle.txt"
+    result_dict_list = get_kindle_highlights(out_put_header, input_file_path, output_intermediate)
+    print("Today is amazing!")
+    
+    ### check if the output format is correct
+    file = open(input_file_path, "r")
+    nr_highlights = file.read().count("==========")
+    
+    for key, element in result_dict_list.items():
+        if len(element) !=nr_highlights:
+            print(f'Number of {key} collected is {len(element)}, which is not consistent with numebr of highlights')
+        
+    
+                
+                
+                
+                
